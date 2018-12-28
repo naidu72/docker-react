a5a9d9e5935ee688cd90c5b3a7bcb476cf1ac8cb
Naidu72 <naidug09@gmail.com>
2018-12-28 15:26:09 +0530

    Revert "added feature brach to Teju"

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Hello!!! *******This is TejendraNaidu****** !
+           Hello!!! *******This is Naidu****** !
           </p>
           <a
             className="App-link"
